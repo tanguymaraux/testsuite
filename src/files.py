c0dbb3b13109e7db46eeed4834f9a31ed9fd6684
Tanguy <tanguy.maraux@epita.fr>
2022-05-05 23:24:45 +0200

    feat(list_files): add function to list yml files

---
+++ src/files.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+
+import yaml
+from dacite import from_dict
+
+
+def list_files(path='tests'):
+    cur_path = Path(__file__).parent.parent.joinpath(
+        path).absolute()
+    files_list = []
+    for root, dirs, files in os.walk(cur_path):
+        for f in files:
+            if f.endswith(".yaml") or f.endswith(".yml"):
+                files_list.append(os.path.join(root, f))
+
+    return files_list
+
+
+def add_file(files, category):
+    categories, testsuite = [], []
+    cur_path = os.path.dirname(os.path.abspath(__file__))
+    for fi in files:
+        with open(os.path.join(cur_path, fi)) as f:
+            for cat in yaml.load(f, Loader=yaml.SafeLoader):
+                categories.append(from_dict(data_class=Category, data=cat))
+
+    for cat in (cat for cat in categories if (category is None) or (category is not None and cat.category == category)):
+        for t in cat.tests:
+            test = from_dict(data_class=TestCase, data=t)
+            test.category = cat.category
+            if test.file == None:
+                dir_path = tests_path + "/" + test.directory
+                for f in os.listdir(dir_path):
+                    file_path = dir_path + "/" + f
+                    if os.path.isfile(file_path) and os.path.splitext(file_path)[1] == ".tig":
+                        test_copy = copy.deepcopy(test)
+                        test_copy.file = file_path
+                        testsuite.append(test_copy)
+            else:
+                test.file = tests_path + "/" + test.directory + "/" + test.file
+                testsuite.append(test)
+    return testsuite
--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import copy
-import os
 import subprocess as sp
 from argparse import ArgumentParser
 from dataclasses import dataclass, field
@@ -7,9 +6,9 @@ from difflib import unified_diff
 from pathlib import Path
 
 import termcolor
-import yaml
 from alive_progress import alive_bar
-from dacite import from_dict
+
+import files
 
 default_timeout = 1
 
@@ -18,7 +17,9 @@ def parse_arg():
     parser = ArgumentParser('Testsuite')
     parser.add_argument('-b', required=True,
                         help='path to the binary', type=Path, metavar='BINARY_PATH',)
-    parser.add_argument('-t', required=False, metavar='TIMEOUT',
+    parser.add_argument('-p', required=False,
+                        help='path to the tests directory', type=Path, metavar='TESTS_PATH',)
+    parser.add_argument('-t', required=False,   metavar='TIMEOUT',
                         help='timeout of execution (seconds)', type=int)
     parser.add_argument('-c', required=False, metavar='CATEGORY',
                         help='category to test', type=str)
@@ -29,17 +30,19 @@ def parse_arg():
 
 
 if __name__ == "__main__":
-    args = parse_arg()
+    #args = parse_arg()
     # TODO: print credits
 
-    category, binary, printf = args.c, args.b, args.p
-    timeout = default_timeout if args.t is None else args.t
+    #category, binary, printf = args.c, args.b, args.p
+    #timeout = default_timeout if args.t is None else args.t
+    files = files.list_files()
+    print(files)
 
-    print(f"Testing binary: {binary}")
+    #print(f"Testing binary: {binary}")
 
-    files = list_files()
-    testsuite = add_file(files, category)
+    #files = list_files()
+    #testsuite = add_file(files, category)
 
-    print(f"Found {len(testsuite)} tests\n")
+    #print(f"Found {len(testsuite)} tests\n")
 
-    exit(print_tests(testsuite, printf))
+    #exit(print_tests(testsuite, printf))
